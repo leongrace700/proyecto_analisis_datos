9c625649f8063453512edcfa5751e2236eb1aa54
leongrace700 <leongrace700@gmail.com>
2026-08-25 18:54:48 -0500

    Update team member columns in app.py
    Removed fourth column for team members and adjusted layout.

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ with tab_inicio:
    # Integrantes
 st.markdown("## 👥 Integrantes del equipo")
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.info("### 👤 Grace Leon")
@@ -222,8 +222,6 @@ with col2:
 with col3:
     st.info("### 👤 Marco Jimenez")
 
-with col4:
-    st.info("### 👤 Zurley Taborda")
 
     st.markdown("---")
 
