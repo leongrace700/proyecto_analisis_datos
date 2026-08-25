9d3d96e673ffc6878dd53f1d577701e6990c6e86
leongrace700 <leongrace700@gmail.com>
2026-08-25 18:51:53 -0500

    Reduce number of columns from 4 to 3

--- app.py
+++ app.py
@@ -211,7 +211,7 @@ with tab_inicio:
    # Integrantes
 st.markdown("## 👥 Integrantes del equipo")
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.info("### 👤 Grace Leon")
